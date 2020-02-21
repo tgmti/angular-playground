ff4ca8158603f089dbb4931404a1b3ab60ee239f
Thiago Mota <tgmspawn@gmail.com>
2020-02-21 14:29:05 -0300

    Dashboar Home Page
    ng g m dashboard --route 'home' --module app.module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,9 @@ import { Routes, RouterModule } from '@angular/router';
 
 
 const routes: Routes = [
-  { path: 'endereco', loadChildren: () => import('./endereco/endereco.module').then(m => m.EnderecoModule) }
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'endereco', loadChildren: () => import('./endereco/endereco.module').then(m => m.EnderecoModule) },
+  { path: 'home', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule) }
 ];
 
 @NgModule({
